34dc3178b12b389127a6df7eed8e0fdbab9ed46f
nevets699 <abrahamcajas90@gmail.com>
2024-11-27 16:08:00 -0500

    help

--- Fundamentos.py
+++ Fundamentos.py
@@ -38,3 +38,23 @@ mensaje2 = 'Fran'
 suma_mensajes = mensaje1 + ', ' + mensaje2 + '. Welcome!'
 print (suma_mensajes)
 
+"""No es recomendable usar muchos + (suma) para un código debido al espacio que va a utilizar, podemos optar por otras opciones"""
+
+suma_mensajes2 = "{}, {}. Welcome!".format(mensaje1, mensaje2)
+print(suma_mensajes2)
+
+suma_mensajes3 = "{}, {}, {}. Acabó la linea de código".format(mensaje, mensaje1, mensaje2)
+print(suma_mensajes3)
+
+# Sacando el método formant y reemplazando directamente las funciones dentro de las cadenas
+
+suma_mensajes5 = f"{mensaje1.lower()}, {mensaje2.upper()}. Welcome!"
+print(suma_mensajes5)
+
+"""Dir nos lista todos los atributos y métodos que tenemos para usar"""
+
+print(dir(mensaje))
+
+"""Help str"""
+
+print(help(str.lower))
